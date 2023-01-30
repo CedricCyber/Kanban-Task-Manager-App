83ace54f064f6827b21910d2fbdb9bdffab11e09
CedricCyber <104404794+CedricCyber@users.noreply.github.com>
2023-01-29 20:54:47 -0600

    added responsiveness to containers

--- src/App.jsx
+++ src/App.jsx
@@ -208,33 +208,133 @@ export default function App() {
     e.preventDefault();
   };
   return (
-    <div className="">
-      {/* TitleBar Section */}
-      <div className="flex h-97">
-        <div className="flex items-center border-bottom">
-          <div className="w-300">
-            <div className="flex justify-center">
-              <img src={logoDark}></img>
+    // ------------------------------------------- JSX ----------------------------------------->
+    <div>
+      <div className={taskFormShown ? "dim" : "light"}>
+        {/* TitleBar Section */}
+        <div className="flex h-97">
+          <div className="flex items-center border-bottom">
+            <div className="w-300">
+              <div className="flex justify-center">
+                <img src={logoDark}></img>
+              </div>
+            </div>
+          </div>
+          <div className="flex w-100 justify-between items-center border-left border-bottom">
+            <h1 className="ml-25">{taskBoards[currentIndex].name}</h1>
+            <div>
+              <button
+                type="button"
+                onClick={toggleTaskForm}
+                className="task-button background-purple text-white mr-20 "
+              >
+                + Add New Task
+              </button>
+              <img className="mr-25" src={settingsIcon} />
             </div>
           </div>
         </div>
-        <div className="flex w-100 justify-between items-center border-left border-bottom">
-          <h1 className="ml-25">{taskBoards[currentIndex].name}</h1>
-          <div>
+
+        <div className="flex">
+          {/* Task Boards Section */}
+          <div className="w-300">
+            <p>
+              ALL BOARDS {"("}
+              {taskBoards.length}
+              {")"}
+            </p>
+            {/* Displays Each Task Board from taskBoards array wrapped
+           with a Link to render the Task Board with React Router*/}
+            {taskBoards.map((board, index) => {
+              // function changeSvgColor() {
+              //   if (svgColor === "grey") return setSvgColor("white");
+              //   if (svgColor === "white") return setSvgColor("grey");
+              // }
+              return (
+                <Link key={index} to={`/${board.name}`}>
+                  <button
+                    // onMouseEnter={changeSvgColor}
+                    // onMouseLeave={changeSvgColor}
+                    onClick={() => {
+                      updateCurrentIndex(index);
+                    }}
+                    className="board-button mt-5"
+                  >
+                    <div className=" ml-25 flex items-center">
+                      {/* <img className="mr-15" src={boardImagePurple} /> */}
+                      {/* {svgColor == "grey" ? (
+                      <img className="mr-15" src={boardImageGrey} />
+                    ) : (
+                      <img className="mr-15" src={boardImageWhite} />
+                    )} */}
+                      {board.name}
+                    </div>
+                  </button>
+                </Link>
+
+                // <button
+                //   onClick={() => {
+                //     deleteBoard(index), console.log(index);
+                //   }}
+                // >
+                //   Delete Board
+                // </button>
+              );
+            })}
+
             <button
-              type="button"
-              onClick={toggleTaskForm}
-              className="task-button background-purple text-white mr-20 "
+              // onMouseEnter={changeSvgColor}
+              // onMouseLeave={changeSvgColor}
+              className="board-button "
+              onClick={addBoard}
             >
-              + Add New Task
+              <div>
+                {/* <img className="mr-15" src={boardImagePurple} /> */}
+                {/* {svgColor == "grey" ? (
+                <img className="mr-15" src={boardImageGrey} />
+                ) : (
+                <img className="mr-15" src={boardImageWhite} />
+              )} */}
+                Add Board
+              </div>
             </button>
-            <img className="mr-25" src={settingsIcon} />
+          </div>
+          {/* Task Lists Section */}
+          <div className="flex background-dark-white task-column-container">
+            <Routes>
+              {/* Creates task lists for each Task Board */}
+              {taskBoards.map((board, index) => {
+                return (
+                  <Route
+                    key={index}
+                    path={`${board.name}`}
+                    element={
+                      <TaskList
+                        columnsProp={board.columns}
+                        // addColumn={addColumn}
+                      />
+                    }
+                  />
+                );
+              })}
+            </Routes>
+            <button
+              onClick={addColumn}
+              className="background-light-grey new-column-button"
+            >
+              <h2 className="text-grey ">+ New Column</h2>
+            </button>
           </div>
         </div>
       </div>
+      {/* ------------------------------------------- Add Task Form ----------------------------------------->*/}
       <form
         onSubmit={handleSubmit}
-        className="absolute add-task-menu flex-col items-center"
+        className={
+          taskFormShown
+            ? "show absolute add-task-menu flex-col items-center"
+            : " hide absolute add-task-menu flex-col items-center"
+        }
       >
         <div id="addTaskMenu" className="w-416 flex-col">
           <h2 className="mt-20">Add New Task</h2>
@@ -268,189 +368,6 @@ export default function App() {
           </button>
         </div>
       </form>
-      <div className="flex">
-        {/* Task Boards Section */}
-        <div className="w-300">
-          <p>
-            ALL BOARDS {"("}
-            {taskBoards.length}
-            {")"}
-          </p>
-          {/* Displays Each Task Board from taskBoards array wrapped
-           with a Link to render the Task Board with React Router*/}
-          {taskBoards.map((board, index) => {
-            // function changeSvgColor() {
-            //   if (svgColor === "grey") return setSvgColor("white");
-            //   if (svgColor === "white") return setSvgColor("grey");
-            // }
-            return (
-              <Link key={index} to={`/${board.name}`}>
-                <button
-                  // onMouseEnter={changeSvgColor}
-                  // onMouseLeave={changeSvgColor}
-                  onClick={() => {
-                    updateCurrentIndex(index);
-                  }}
-                  className="board-button mt-5"
-                >
-                  <div className=" ml-25 flex items-center">
-                    {/* <img className="mr-15" src={boardImagePurple} /> */}
-                    {/* {svgColor == "grey" ? (
-                      <img className="mr-15" src={boardImageGrey} />
-                    ) : (
-                      <img className="mr-15" src={boardImageWhite} />
-                    )} */}
-                    {board.name}
-                  </div>
-                </button>
-              </Link>
-
-              // <button
-              //   onClick={() => {
-              //     deleteBoard(index), console.log(index);
-              //   }}
-              // >
-              //   Delete Board
-              // </button>
-            );
-          })}
-
-          <button
-            // onMouseEnter={changeSvgColor}
-            // onMouseLeave={changeSvgColor}
-            className="board-button "
-            onClick={addBoard}
-          >
-            <div>
-              {/* <img className="mr-15" src={boardImagePurple} /> */}
-              {/* {svgColor == "grey" ? (
-                <img className="mr-15" src={boardImageGrey} />
-              ) : (
-                <img className="mr-15" src={boardImageWhite} />
-              )} */}
-              Add Board
-            </div>
-          </button>
-        </div>
-        {/* Task Lists Section */}
-        <div className="flex background-dark-white task-column-container">
-          <Routes>
-            {/* Creates task lists for each Task Board */}
-            {taskBoards.map((board, index) => {
-              return (
-                <Route
-                  key={index}
-                  path={`${board.name}`}
-                  element={
-                    <TaskList
-                      columnsProp={board.columns}
-                      // addColumn={addColumn}
-                    />
-                  }
-                />
-              );
-            })}
-          </Routes>
-          <button
-            onClick={addColumn}
-            className="background-light-grey new-column-button"
-          >
-            <h2 className="text-grey ">+ New Column</h2>
-          </button>
-        </div>
-      </div>
     </div>
   );
 }
-
-/* <input id="newArrayItem" type={"text"}></input>
-{console.log(newArrayItem)}
-<button onClick={getInput}>Submit</button>
-{array.map((fruit) => {
-  return (
-    <Link to={fruit}>
-      <h1>{fruit}</h1>
-    </Link>
-  );
-})}
-<Link to="/">
-  <button>HELLO I'M A BUTTON</button>
-</Link>
-<img src={addTask} />
-
-<Link to="/route2">
-  <button>HELLO I'M A BUTTON AS WELL</button>
-</Link>
-<button onClick={deleteItem}>
-  I'M A BUTTON THAT DELETES AN ITEM IN THE ARRAY
-</button>
-<nav>
-  <ul>
-    {data.boards.map((boards) => {
-      return <li>{boards.name}</li>;
-    })}
-  </ul>
-</nav>
-{console.log(data.boards)}
-<Routes>
-  <Route path="/" element={<Route1 />} />
-  <Route path="/route2" element={<Route2 />} />
-  {array.map((fruit) => {
-    return <Route path={fruit} element={<h2>{fruit}</h2>} />;
-  })}
-</Routes> */
-
-// Todo Tasks (Rough Draft)
-// function TodoTasks(data) {
-//   const [task, setTask] = data.map((task) => {
-//     if (data.status == todo) {
-//       return (
-//         <div>
-//           <h2>{task.h2}</h2>
-//           <p>{task.p}</p>
-//         </div>
-//       );
-//     }
-//   });
-//   return (
-//     <div>
-//       <h3>Todo</h3>
-//       {task}
-//     </div>
-//   );
-// }
-// TaskBar (RoughDraft)
-// function TaskBar(){
-//   return (
-
-//   )
-// }
-// TaskBoard Component (Rough Draft)
-// function TaskBoard(){
-//   return (
-//     <div>
-//       <TaskBar/>
-//       <TodoTasks/>
-//       <DoingTasks/>
-//       <DoneTasks/>
-//       <NewTask/>
-//     </div>
-//   )
-// }
-// TitleBar Component(Rough Draft)
-// function TitleBar() {
-//   return (
-//     <div>
-//       <div>
-//         <Logo></Logo>
-//       </div>
-//       <div>
-//         <Title />
-//         <div>
-//           <Button />
-//           <Settings />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
